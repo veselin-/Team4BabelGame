0590123800816df5ecb403f94e07a32db4450cdd
Tomas Moesgaard <tomahawk@os.dk>
2015-10-27 15:43:59 +0100

    Added UI prototype scene

---
+++ Babel/Assets/UI/Scripts/DragHandler.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.EventSystems;
+
+public class DragHandler : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
+{
+    public static GameObject SymbolBeingDragged;
+    private Vector3 _startPosition;
+    public static Transform _startParent;
+
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        SymbolBeingDragged = gameObject;
+        
+        _startPosition = transform.position;
+        _startParent = transform.parent;
+        GetComponent<CanvasGroup>().blocksRaycasts = false;
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        transform.position = Input.mousePosition;
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+
+        SymbolBeingDragged = null;
+        GetComponent<CanvasGroup>().blocksRaycasts = true;
+        if (transform.parent == _startParent)
+        {
+            transform.position = _startPosition;
+        }
+    }
+}

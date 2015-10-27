e2ef1a27647b93b5d1095b014aa6c22b4768bb64
Veselin <veselqshko@gmail.com>
2015-10-27 12:29:34 +0100

    Orthograpic Isometric and Perspective Isometric cameras

---
+++ Babel/Assets/Environment/Prototypes/CameraViews/CameraFollow.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollow : MonoBehaviour {
+
+    public Transform Target;
+    public float speed = 1f;
+    private Vector3 velocity = Vector3.zero;
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+       
+        transform.position = Vector3.SmoothDamp(transform.position, Target.position, ref velocity, speed * Time.deltaTime);
+	}
+}

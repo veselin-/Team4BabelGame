f6b8bc0e9f34d373e200830acc1ad8762a1ee315
Peter Troelsen <prtroelsen@gmail.com>
2015-10-27 13:19:05 +0100

    Added a first version of the AI of the sidekick, also added a few demo materials like a door and some basic player movement

---
+++ Babel/Assets/Characters/SideKick/Scripts/EnemyMovement.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace Assets.Characters.SideKick.Scripts
+{
+    public class EnemyMovement : MonoBehaviour
+    {
+        public IState CurrentState;
+
+        // Use this for initialization
+        void Start ()
+        {
+            StartCoroutine(StateExecuter());
+        }
+
+        IEnumerator StateExecuter()
+        {
+            var explorerState = new ExploreState(GetComponent<NavMeshAgent>());
+            CurrentState = explorerState;
+
+            while (true)
+            {
+                yield return new WaitForSeconds(0.1f);
+                if (CurrentState == null || CurrentState.IsDoneExecuting())
+                    CurrentState = explorerState;
+                
+                CurrentState.ExecuteState();
+            }
+        }
+
+        public void AssignNewState(IState state)
+        {
+            CurrentState = state;
+        }
+    }
+}
---
+++ Babel/Assets/Core/NavMesh/NavMeshExtention.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+namespace Assets.Core.NavMesh
+{
+    public static class NavMeshExtention {
+
+        public static bool HasReachedTarget(this NavMeshAgent mNavMeshAgent)
+        {
+            return mNavMeshAgent.remainingDistance < 1f;
+            //if (mNavMeshAgent.pathPending) return false;
+            //return mNavMeshAgent.remainingDistance <= mNavMeshAgent.stoppingDistance &&
+            //       (!mNavMeshAgent.hasPath || mNavMeshAgent.velocity.sqrMagnitude < 1f);
+        }
+    }
+}

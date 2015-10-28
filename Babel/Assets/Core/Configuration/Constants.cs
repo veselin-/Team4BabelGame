aeae5c33a0959615353a4af243ef728d709d2516
Peter Troelsen <prtroelsen@gmail.com>
2015-10-28 12:25:32 +0100

    Added some tags, for some of the more objects

--- Babel/Assets/Core/Configuration/Constants.cs
+++ Babel/Assets/Core/Configuration/Constants.cs
@@ -11,6 +11,10 @@ namespace Assets.Core.Configuration
             public const string GameMaster = "GameMaster";
             public const string SideKick = "SideKick";
             public const string Player = "Player";
+            public const string Stick = "Stick";
+            public const string Key = "Key";
+            public const string Torch = "Torch";
+            public const string Bottle = "Bottle";
         }
     }
 }

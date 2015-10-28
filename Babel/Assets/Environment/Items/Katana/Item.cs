57fd6790ada9737351e2d8f67b365d25c4c11b69
Anders Wessberg <anders_wess@hotmail.com>
2015-10-28 12:38:26 +0100

    Player can pick up katana, and drop it again, by pressing the player.

---
+++ Babel/Assets/Environment/Items/Katana/Item.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Item : MonoBehaviour {
+
+    public static bool pressed = false;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnMouseDown()
+    {
+        pressed = true;
+        Debug.Log("THIS IS NOW PICKED UP");
+    }
+}

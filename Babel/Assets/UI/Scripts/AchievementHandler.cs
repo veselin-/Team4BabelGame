1861b623d881ef0d35049f8cffee96cc6bb8b1f0
Anders Wessberg <anders_wess@hotmail.com>
2015-10-28 16:11:37 +0100

    Achievement canvas

---
+++ Babel/Assets/UI/Scripts/AchievementHandler.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class AchievementHandler : MonoBehaviour {
+
+    //public Sprite s1;
+    //public Image a1;
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+}

61eb5841ed28746ee6ae47c60ad5e4bd549a9fe6
Anders Wessberg <anders_wess@hotmail.com>
2015-10-30 14:12:40 +0100

    Made level01 functional with the player and the AI.

--- Babel/Assets/Environment/Stairs/Scripts/StairMovement.cs
+++ Babel/Assets/Environment/Stairs/Scripts/StairMovement.cs
@@ -6,9 +6,9 @@ using Assets.Core.InteractableObjects;
 using Assets.Core.LevelMaster;
 using UnityEngine;
 
-namespace Assets.Environment.Doors.SlidingDemoDoor.Scripts
+namespace Assets.Environment.Stairs.Scripts
 {
-    public class DoorMovement : MonoBehaviour
+    public class StairMovement : MonoBehaviour
     {
 
         public GameObject[] Interactables;
@@ -18,7 +18,7 @@ namespace Assets.Environment.Doors.SlidingDemoDoor.Scripts
 
         private Vector3 _startPosition;
         private Vector3 _endPosistion;
-        private bool _doorIsOpen;
+        private bool _stairIsShown;
     
 
         // Use this for initialization
@@ -26,7 +26,7 @@ namespace Assets.Environment.Doors.SlidingDemoDoor.Scripts
         {
             // Sliding positions
             _startPosition = transform.localPosition;
-            _endPosistion = new Vector3(_startPosition.x, _startPosition.y, _startPosition.z - transform.localScale.z);
+            _endPosistion = new Vector3(_startPosition.x, _startPosition.y, _startPosition.z + transform.localScale.z + 2);
 
             // Get all IInteractables
             _interactables = new List<IInteractable>();
@@ -38,16 +38,16 @@ namespace Assets.Environment.Doors.SlidingDemoDoor.Scripts
 
         void Update()
         {
-            transform.localPosition = Vector3.MoveTowards(transform.localPosition, _doorIsOpen ? _endPosistion : _startPosition, Time.deltaTime * MovementSpeed);
+            transform.localPosition = Vector3.MoveTowards(transform.localPosition, _stairIsShown ? _endPosistion : _startPosition, Time.deltaTime * MovementSpeed);
         }
 
         IEnumerator CheckForInputs()
         {
-            while (!_doorIsOpen)
+            while (!_stairIsShown)
             {
                 if (_interactables.All(i => i.HasBeenActivated()))
                 {
-                    _doorIsOpen = true;
+                    _stairIsShown = true;
                     //GameObject.FindGameObjectWithTag(Constants.Tags.GameMaster).GetComponent<RoomManager>().SetCurrentRoom(1);
                 }   
                 yield return new WaitForSeconds(0.2f);

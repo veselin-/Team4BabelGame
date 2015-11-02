607b01fbe7dc680e25568b5019bdf44a8d88bf10
Anders Wessberg <anders_wess@hotmail.com>
2015-11-02 16:00:45 +0100

    levers and plankbridges work. no door in level02

---
+++ Babel/Assets/Environment/Plank/Scripts/PlankMovement.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using Assets.Core.Configuration;
+using Assets.Core.InteractableObjects;
+using Assets.Core.LevelMaster;
+using UnityEngine;
+
+namespace Assets.Environment.Plank.Scripts
+{
+    public class PlankMovement : MonoBehaviour
+    {
+
+        public GameObject[] Interactables;
+        public float MovementSpeed;
+
+        private List<IInteractable> _interactables;
+
+        private Vector3 _startPosition;
+        private Vector3 _endPosistion;
+        private bool _plankIsShown;
+
+        public GameObject obstacle;
+
+        // Use this for initialization
+        void Start()
+        {
+            // Sliding positions
+            _startPosition = transform.localPosition;
+            _endPosistion = new Vector3(_startPosition.x, _startPosition.y + transform.localScale.y + 2, _startPosition.z);
+
+            // Get all IInteractables
+            _interactables = new List<IInteractable>();
+            foreach (var interactable in Interactables)
+                _interactables.Add(interactable.GetComponent<IInteractable>());
+
+            StartCoroutine(CheckForInputs());
+        }
+
+        void Update()
+        {
+            transform.localPosition = Vector3.MoveTowards(transform.localPosition, _plankIsShown ? _endPosistion : _startPosition, Time.deltaTime * MovementSpeed);
+        }
+
+        IEnumerator CheckForInputs()
+        {
+            while (true)
+            {
+                if (_interactables.All(i => i.HasBeenActivated()))
+                {
+                    _plankIsShown = true;
+                    obstacle.SetActive(false);
+                    //GameObject.FindGameObjectWithTag(Constants.Tags.GameMaster).GetComponent<RoomManager>().SetCurrentRoom(1);
+                }
+                else
+                {
+                    _plankIsShown = false;
+                    obstacle.SetActive(true);
+                }
+                yield return new WaitForSeconds(0.2f);
+            }
+        }
+    }
+}
+

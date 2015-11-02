d5c590b76734475c0d195ee7ff5039126957d05b
Tomas Moesgaard <tomahawk@os.dk>
2015-11-02 15:25:22 +0100

    more tweaks to the sign system

--- Babel/Assets/UI/Scripts/SymbolHandler.cs
+++ Babel/Assets/UI/Scripts/SymbolHandler.cs
@@ -10,6 +10,7 @@ public class SymbolHandler : DragHandler
 
     public Image Image1;
     public Image Image2;
+    public Image Image3;
 
     private DatabaseManager databaseManager;
 
@@ -20,12 +21,7 @@ public class SymbolHandler : DragHandler
         databaseManager =
             GameObject.FindGameObjectWithTag(Constants.Tags.DatabaseManager).GetComponent<DatabaseManager>();
 
-        Sign w = databaseManager.GetSign(ID);
-
-        Syllable s1 = databaseManager.GetSyllable(w.SyllableSequence[0]);
-        Syllable s2 = databaseManager.GetSyllable(w.SyllableSequence[1]);
-
-        SetSyllables(s1.ImageName, s2.ImageName);
+        UpdateSymbol();
 
     }
 
@@ -39,7 +35,45 @@ public class SymbolHandler : DragHandler
     public void SetSyllables(string syl1, string syl2)
     {
         Image1.sprite = databaseManager.GetImage(syl1);
+        Image1.color = Color.white;
         Image2.sprite = databaseManager.GetImage(syl2);
+        Image2.color = Color.white;
+        Image3.color = Color.clear;
+    }
+
+    public void SetSyllables(string syl1, string syl2, string syl3)
+    {
+        Image1.sprite = databaseManager.GetImage(syl1);
+        Image1.color = Color.white;
+        Image2.sprite = databaseManager.GetImage(syl2);
+        Image2.color = Color.white;
+        Image3.sprite = databaseManager.GetImage(syl3);
+        Image3.color = Color.white;
+    }
+
+    public void UpdateSymbol()
+    {
+        Sign s = databaseManager.GetSign(ID);
+
+        if (s == null)
+            return;
+
+
+        if (s.SyllableSequence.Count == 2)
+        {
+            Syllable s1 = databaseManager.GetSyllable(s.SyllableSequence[0]);
+            Syllable s2 = databaseManager.GetSyllable(s.SyllableSequence[1]);
+            SetSyllables(s1.ImageName, s2.ImageName);
+        }
+        else if (s.SyllableSequence.Count == 3)
+        {
+            Syllable s1 = databaseManager.GetSyllable(s.SyllableSequence[0]);
+            Syllable s2 = databaseManager.GetSyllable(s.SyllableSequence[1]);
+            Syllable s3 = databaseManager.GetSyllable(s.SyllableSequence[2]);
+            SetSyllables(s1.ImageName, s2.ImageName, s3.ImageName);
+        }
+
+
     }
 
 }

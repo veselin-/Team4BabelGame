e2d77f89ec7774cb1aed793b50d7986a02eca21e
Bo Olsen <cwg437@alumni.ku.dk>
2015-11-02 15:19:14 +0100

    We don't load the DB async.

--- Babel/Assets/Prototype stuff/TestDatabaseManager/TestingDBManager.cs
+++ Babel/Assets/Prototype stuff/TestDatabaseManager/TestingDBManager.cs
@@ -11,23 +11,11 @@ public class TestingDBManager : MonoBehaviour {
 	void Start () {
         db = GameObject.Find("DatabaseManager").GetComponent<DatabaseManager>();
         db.LoadData();
-
-        StartCoroutine(WaitAndPrint(0.5F));
-    }
-
-    IEnumerator WaitAndPrint(float waitTime)
-    {
-       while(!db.DatabasesLoaded())
-        {
-            Debug.Log("not loaded");
-            yield return new WaitForSeconds(waitTime);
-        }
-
-        Debug.Log("LOADED");
-
         GameObject.Find("Image0").GetComponent<Image>().sprite = db.GetImage(db.GetSyllable(0).ImageName);
         GameObject.Find("Image1").GetComponent<Image>().sprite = db.GetImage(db.GetSyllable(1).ImageName);
         GameObject.Find("Image2").GetComponent<Image>().sprite = db.GetImage(db.GetSyllable(2).ImageName);
         GameObject.Find("Image3").GetComponent<Image>().sprite = db.GetImage(db.GetSyllable(3).ImageName);
+
     }
+
 }

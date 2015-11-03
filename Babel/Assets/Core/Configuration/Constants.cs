265a3ba1daf17865eff6a3c43080173e9bb75051
Peter Troelsen <prtroelsen@gmail.com>
2015-11-03 14:02:38 +0100

    Made it so the player and sidekick can get hats! :)

--- Babel/Assets/Core/Configuration/Constants.cs
+++ Babel/Assets/Core/Configuration/Constants.cs
@@ -28,5 +28,10 @@ namespace Assets.Core.Configuration
             public const string Signs = "Signs.xml";
             public const string Sentences = "Sentences.xml";
         }
+
+        public class ShopItems
+        {
+            public const string Hat = "Hat";
+        }
     }
 }

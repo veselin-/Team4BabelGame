baa74b40a2ef12eb4922ef431a0806e98c324877
Peter Troelsen <prtroelsen@gmail.com>
2015-11-03 14:38:58 +0100

    Made it so the gamemaster spawns the player and sidekick. Made the order canvas a prefab Applied all this to Level01

---
+++ Babel/Assets/Core/GameMaster/Scripts/SpawnPlayers.cs
@@ -0,0 +1,36 @@
+﻿using Assets.Characters.Player.Scripts;
+using Assets.Characters.SideKick.Scripts;
+using Assets.Core.Configuration;
+using UnityEngine;
+
+namespace Assets.Core.GameMaster.Scripts
+{
+    public class SpawnPlayers : MonoBehaviour
+    {
+        public GameObject CharactorPrefab;
+
+        public Transform PlayerSpawnPoint;
+        public Transform SidekickSpawnPoint;
+        
+        // Use this for initialization
+        void Start ()
+        {
+            var sidekick = Instantiate(CharactorPrefab);
+            var player = Instantiate(CharactorPrefab);
+
+            sidekick.transform.position = SidekickSpawnPoint.position;
+            player.transform.position = PlayerSpawnPoint.position;
+
+            sidekick.tag = Constants.Tags.SideKick;
+            sidekick.name = Constants.Tags.SideKick;
+            sidekick.GetComponent<PlayerMovement>().enabled = false;
+
+            player.GetComponent<SidekickControls>().enabled = false;
+        }
+
+        // Update is called once per frame
+        void Update () {
+	
+        }
+    }
+}

ee28715a473723c6d504fbe6adc0d81c90857010
Peter Troelsen <prtroelsen@gmail.com>
2015-11-04 08:49:58 +0100

    First version of the end points

---
+++ Babel/Assets/Characters/AiScripts/States/EndGameState.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+namespace Assets.Characters.AiScripts.States
+{
+    public class EndGameState : MonoBehaviour, IState {
+
+        public float WaitingTime { get; set; }
+        public void ExecuteState()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public bool IsDoneExecuting()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ Babel/Assets/Core/GameMaster/Scripts/EndPoints.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+namespace Assets.Core.GameMaster.Scripts
+{
+    public class EndPoints : MonoBehaviour
+    {
+
+        public GameObject[] Endpoints;
+
+        // Use this for initialization
+        void Start () {
+	
+        }
+	
+        // Update is called once per frame
+        void Update () {
+
+
+        }
+
+        void OnTriggerEnter(Collider other)
+        {
+            Debug.Log(other.tag);    
+        }
+    }
+}

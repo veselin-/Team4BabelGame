e834a6d450b8cf705aacaf7d7e54baf0595b2111
Peter Troelsen <prtroelsen@gmail.com>
2015-11-09 16:43:26 +0100

    Redid the torurial level 1, and added the rigid body back onto the player prefab

--- Babel/Assets/Characters/AiScripts/ScriptedBehaviours/Toturial01Behaviour.cs
+++ Babel/Assets/Characters/AiScripts/ScriptedBehaviours/Toturial01Behaviour.cs
@@ -1,4 +1,6 @@
-﻿using Assets.Characters.SideKick.Scripts;
+﻿using System;
+using System.Collections.Generic;
+using Assets.Characters.SideKick.Scripts;
 using Assets.Core.Configuration;
 using UnityEngine;
 
@@ -6,10 +8,12 @@
 {
     public class Toturial01Behaviour : MonoBehaviour
     {
-        private State state;
-        private bool waitingForPlayer;
-        private float movementSpeed;
-        private NavMeshAgent _agent;
+        private State _state;        private bool _waitingForPlayer;        private float _movementSpeed;        private NavMeshAgent _agent;
+
+        private Transform waypoint01;
+        private Transform waypoint02;
+
+        private InteractableSpeechBubble bubble;
 
         // Use this for initialization
         void Start ()
@@ -17,37 +21,34 @@
             var colider = gameObject.AddComponent<SphereCollider>();
             colider.radius = 5;            colider.isTrigger = true;            _agent = GetComponent<NavMeshAgent>();            GetComponent<SidekickControls>().enabled = false;
             var aiMovement = GetComponent<AiMovement>();
-            aiMovement.StrollSpeed = 0;            movementSpeed = aiMovement.MovementSpeed;
+            aiMovement.StrollSpeed = 0;            _movementSpeed = aiMovement.MovementSpeed;
             _agent.destination = _agent.transform.position;
-            waitingForPlayer = true;        }
-	
+            _waitingForPlayer = true;
+
+            var waypoints = GameObject.FindGameObjectWithTag(Constants.Tags.GameMaster).transform.FindChild("Waypoints");
+            waypoint01 = waypoints.FindChild("waypoint");
+            waypoint02 = waypoints.FindChild("waypoint (1)");
+
+            bubble = FindObjectOfType<InteractableSpeechBubble>();        }
+
         // Update is called once per frame
         void Update () {
-            _agent.speed = movementSpeed;
-                        if (state == State.First && Vector3.Distance(_agent.transform.position, new Vector3(17, 1, 17)) < 2)            {
+            _agent.speed = _movementSpeed;
+                        if (_state == State.First && Vector3.Distance(_agent.transform.position, waypoint01.position) < 2)            {
                 // TODO: waving animation
-                // TODO: Say symbol: 8
-                Debug.Log("Say symbol: 8");
-                waitingForPlayer = true;
-                state = State.Second;            } else if (state == State.Second && Vector3.Distance(_agent.transform.position, new Vector3(35, 1, -8)) < 2)            {                // TODO: waving animation
-                // TODO: Say symbol: 8
-                Debug.Log("Say symbol: 8");                state = State.Thrid;                waitingForPlayer = true;            }        }
+                bubble.ActivateSidekickSignBubble(new List<int>(new[] { 8 }));                _waitingForPlayer = true;
+                _state = State.Second;            } else if (_state == State.Second && Vector3.Distance(_agent.transform.position, waypoint02.position) < 2)            {
+                // TODO: waving animation                bubble.ActivateSidekickSignBubble(new List<int>(new[] { 8 }));                _state = State.Thrid;                _waitingForPlayer = true;            }        }
                 void OnTriggerStay(Collider other)        {
-            if(other.tag != Constants.Tags.Player || !waitingForPlayer) return;            waitingForPlayer = false;            switch (state)            {
+            if(other.tag != Constants.Tags.Player || !_waitingForPlayer) return;            _waitingForPlayer = false;            switch (_state)            {
                 case State.First:
                     // TODO: waving animation
                     CreateNewSymbol.SymbolID = 8;
                     GameObject.FindGameObjectWithTag(Constants.Tags.GameUI).GetComponent<UIControl>().SignCreationEnter();
-                    _agent.destination = new Vector3(17,1,17);                    _agent.speed = movementSpeed;
+                    _agent.destination = waypoint01.position;                    _agent.speed = _movementSpeed;
                     break;
                 case State.Second:
-                    _agent.destination = new Vector3(35, 1, -8);                    _agent.speed = movementSpeed;
-                    break;
-                case State.Thrid:
-                    if (Vector3.Distance(_agent.transform.position, new Vector3(35, 1, -8)) > 2) return;
-                    Debug.Log("Say symbol: 8");
-                    // TODO: waving animation
-                    // TODO: Say symbol: 8
+                    _agent.destination = waypoint02.position;                    _agent.speed = _movementSpeed;
                     break;
             }
         }

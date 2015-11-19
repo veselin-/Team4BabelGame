1433c2cbd4cb996dedff68570bc0fd626630bdee
Peter Troelsen <prtroelsen@gmail.com>
2015-11-19 16:07:58 +0100

    Added the Google achievements plugin

---
+++ Babel/Assets/Core/Achievements/AchievementManager.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using Assets.Core.Configuration;
+using GooglePlayGames;
+using UnityEngine.SocialPlatforms;
+using UnityEngine;
+
+namespace Assets.Core.Achievements
+{
+    public class AchievementManager
+    {
+
+        private static AchievementManager _instance;
+        public static AchievementManager Instance
+        {
+            get
+            {
+                _instance = _instance ?? new AchievementManager();
+                return _instance;
+            }
+        }
+
+        private AchievementManager()
+        {
+            var _windowHandler = GameObject.FindGameObjectWithTag(Constants.Tags.WindowManager).GetComponent<WindowHandler>();
+            try
+            {
+                PlayGamesPlatform.DebugLogEnabled = true;
+                PlayGamesPlatform.Activate();
+
+                _windowHandler.ActivateDialogWindow("Test", Social.localUser.ToString(), true);
+
+                Social.localUser.Authenticate((bool success) =>
+                {
+                    if (success)
+                    {
+                        Debug.Log("Sign in Successful");
+                    }
+                    else
+                    {
+                        Debug.LogError("Sign in Unsuccessful");
+                        _windowHandler.ActivateDialogWindow("Error", "Sign in Unsuccessful", true);
+                    }
+                });
+            }
+            catch (Exception e)
+            {
+               
+            }
+
+        }
+
+        public void UnlockAchievement(string achievement)
+        {
+            try
+            {
+                if (Social.localUser.authenticated)
+                {
+                    Social.ReportProgress(achievement, 100.0f, (bool success) =>
+                    {
+                        //if (success)
+                        //{
+                        //    console.text = "Achievement Unlock Successful";
+                        //}
+                        //else
+                        //{
+                        //    console.text = "Achievement Unlock Unsuccessful";
+                        //    Handheld.Vibrate();
+                        //}
+                    });
+                }
+            }
+            catch (Exception e)
+            {
+                Debug.LogError(e.Message);
+            }
+        }
+    }
+} 

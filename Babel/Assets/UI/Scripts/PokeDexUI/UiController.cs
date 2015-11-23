3b6244f64a99676a3e8636c4c3616f007ed09169
Anders Wessberg <anders_wess@hotmail.com>
2015-11-23 14:41:48 +0100

    new pokedex with hint function

--- Babel/Assets/UI/Scripts/PokeDexUI/UiController.cs
+++ Babel/Assets/UI/Scripts/PokeDexUI/UiController.cs
@@ -13,6 +13,9 @@ public class UiController : MonoBehaviour
     public GameObject _pokedexButton;
     public GameObject closeUiBut;
     public GameObject menuMask;
+    public GameObject MinBut;
+    public GameObject hintPanel;
+
     NavMeshAgent navMeshP, navMeshS;
 	private AudioManager _audioManager;
 	private CameraManager _cameraManager;
@@ -22,6 +25,7 @@ public class UiController : MonoBehaviour
     private float sideKickSpeed;
 	//private GameObject _pokedexButton;
 	private GameObject _pauseCanvas;
+    private GameObject arrowBut;
     int hotbarOpen = 0;
 
     // Use this for initialization
@@ -34,6 +38,7 @@ public class UiController : MonoBehaviour
 		//_pokedexButton = transform.FindChild ("Button").gameObject;
 		_pauseCanvas = GameObject.FindObjectOfType<PauseScreen> ().gameObject;
         anim = GetComponent<Animator>();
+        arrowBut = GameObject.FindGameObjectWithTag("PokedexButton");
     }
 	
 	// Update is called once per frame
@@ -74,6 +79,16 @@ public class UiController : MonoBehaviour
     {
         if (hotbarOpen == 2)
         {
+            if (arrowBut.transform.rotation.z == 0)
+            {
+                Debug.Log("IM IN THE IF SENTENCE");
+                anim.SetTrigger("MenuExit");
+                hotbarOpen = 1;
+                HotbarPokedexOpen();
+                return;
+            }
+            arrowBut.transform.rotation = new Quaternion(0, 0, 0, 0);
+            Debug.Log("I CHANGED THE ROTATION!");
             anim.SetTrigger("MenuToggle");
             _cameraManager.enabled = false;
             _playerMovement.enabled = false;
@@ -81,6 +96,7 @@ public class UiController : MonoBehaviour
             Time.timeScale = 0;
             _pauseCanvas.SetActive(false);
             closeUiBut.SetActive(true);
+            hintPanel.SetActive(true);
             menuMask.GetComponent<ScrollRect>().enabled = true;
         }
     }
@@ -90,28 +106,52 @@ public class UiController : MonoBehaviour
         if(hotbarOpen == 1)
         {
             anim.SetTrigger("MenuToggle");
-            //_cameraManager.enabled = false;
-            //_playerMovement.enabled = false;
+            arrowBut.transform.rotation = new Quaternion(0, 0, 180, 0);
+            _cameraManager.enabled = true;
+            _playerMovement.enabled = true;
             _audioManager.PokedexBtnOpenPlay();
-            //_pauseCanvas.SetActive(false);
+            _pauseCanvas.SetActive(true);
+            closeUiBut.SetActive(false);
+            Time.timeScale = 1;
             menuMask.GetComponent<ScrollRect>().enabled = false;
             menuMask.GetComponent<ScrollRect>().horizontalNormalizedPosition = 0;
+            MinBut.SetActive(true);
+            hintPanel.SetActive(false);
+        }
+    }
+
+    public void hotbarOpenMinusOne()
+    {
+        if(hotbarOpen > 0)
+        {
+            hotbarOpen -= 1;
         }
     }
 
     public void hotbarOpenPlusOne()
     {
-        hotbarOpen += 1;
+        if (hotbarOpen >= 2)
+        {
+            hotbarOpen = 2;
+        }
+        else
+        {
+            hotbarOpen += 1;
+        }
     }
 
     public void PokedexClose()
     {
         anim.SetTrigger("MenuExit");
+        hintPanel.SetActive(false);
         _cameraManager.enabled = true;
 		_playerMovement.enabled = true;
 		_audioManager.PokedexBtnClosePlay ();
-         Time.timeScale = 1;
+        arrowBut.transform.rotation = new Quaternion(0, 0, 180, 0);
+        Time.timeScale = 1;
         hotbarOpen = 0;
 		_pauseCanvas.SetActive (true);
+        MinBut.SetActive(false);
+        Debug.Log(hotbarOpen);
     }
 }
---
+++ Babel/Assets/UI/Test/ButtonForHint.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using UnityEngine.EventSystems;
+
+public class ButtonForHint : MonoBehaviour
+{
+
+    public int ID;
+    public Text hintText;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+    }
+
+    public void GetHintForSign()
+    {
+        switch (ID)
+        {
+            case 0:
+                hintText.text = "whatever";
+                break;
+            case 1:
+                break;
+            case 2:
+                break;
+            case 3:
+                break;
+            case 4:
+                break;
+            case 5:
+                break;
+            case 6:
+                break;
+            case 7:
+                break;
+            case 8:
+                break;
+            case 9:
+                break;
+            case 10:
+                break;
+        }
+    }
+}

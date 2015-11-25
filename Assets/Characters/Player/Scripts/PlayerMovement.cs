7e9d3eda04ae8e2254ad99f5f9fdcdcbbc2e2d01
Veselin <veselqshko@gmail.com>
2015-11-25 20:21:46 +0100

    Player should no longer move when the UI is clicked

--- Assets/Characters/Player/Scripts/PlayerMovement.cs
+++ Assets/Characters/Player/Scripts/PlayerMovement.cs
@@ -31,14 +31,16 @@ namespace Assets.Characters.Player.Scripts
    
 #if UNITY_EDITOR
             if (Input.GetMouseButtonUp(0))
-            {
-                _canMove = true;
+			{
+				if(!EventSystem.current.IsPointerOverGameObject(-1))
+                	_canMove = true;
             }
 #endif
             if (Input.touchCount == 1 )
             {
-                if (Input.GetTouch(0).phase == TouchPhase.Began)
-                    _tap = true;
+				if (Input.GetTouch(0).phase == TouchPhase.Began)
+					if(!EventSystem.current.IsPointerOverGameObject(Input.GetTouch(0).fingerId))
+						_tap = true;
 
                 if (Input.GetTouch(0).phase == TouchPhase.Moved)
                     _tap = false;
@@ -50,27 +52,35 @@ namespace Assets.Characters.Player.Scripts
                 }
             }
 
-            if (!_canMove) return;
-
-            _tap = false;
-            _canMove = false;
-           // if (!Input.GetMouseButtonUp(0)) return;
-
-            var ts = Input.touches;
-            if (ts.Length > 1 || (ts.Length > 0 && EventSystem.current.IsPointerOverGameObject(ts[0].fingerId))
-                || EventSystem.current.IsPointerOverGameObject()) return;
-            
-            // Find all object in ray, and sort them by distance to object.
-            var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-            RaycastHit[] hits = Physics.RaycastAll(ray, 100);
-            hits = hits.OrderBy(h => h.distance).ToArray();
-
-            var state = hits.Select(hit => GameObjectToState(hit.transform.gameObject, hit))
-                    .FirstOrDefault(s => s != null);
-
-            if (state != null)
-                _ai.AssignNewState(state);
         }
+
+		void LateUpdate()
+		{
+			if (!_canMove) return;
+			
+			_tap = false;
+			_canMove = false;
+			// if (!Input.GetMouseButtonUp(0)) return;
+			
+			var ts = Input.touches;
+			
+			Debug.Log ("touches " + EventSystem.current.IsPointerOverGameObject());
+
+			if (ts.Length > 1 || (ts.Length > 0 && EventSystem.current.IsPointerOverGameObject(ts[0].fingerId))
+			    || EventSystem.current.IsPointerOverGameObject())	return;
+			
+			
+			// Find all object in ray, and sort them by distance to object.
+			var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+			RaycastHit[] hits = Physics.RaycastAll(ray, 100);
+			hits = hits.OrderBy(h => h.distance).ToArray();
+			
+			var state = hits.Select(hit => GameObjectToState(hit.transform.gameObject, hit))
+				.FirstOrDefault(s => s != null);
+			
+			if (state != null)
+				_ai.AssignNewState(state);
+		}
 
         IState GameObjectToState(GameObject other, RaycastHit hit)
         {

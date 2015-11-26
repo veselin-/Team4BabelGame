c5ac1b0e52cf43268d5fdb11d15488c42f77cc3c
Anders Wessberg <anders_wess@hotmail.com>
2015-11-26 13:03:53 +0100

    unlock level 7 by buying it

---
+++ Assets/UI/Scripts/LevelSelect/BuyLevelSeven.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+using Assets.Core.Configuration;
+using Assets.Core.LevelSelector;
+
+public class BuyLevelSeven : MonoBehaviour {
+    public GameObject ls;
+	// Use this for initialization
+	void Start () {
+        //wh = GameObject.FindGameObjectWithTag(Constants.Tags.WindowManager).GetComponent<WindowHandler>().
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void DoYouWannaBuy()
+    {
+        GameObject.FindGameObjectWithTag(Constants.Tags.WindowManager).GetComponent<WindowHandler>().CreateConfirmDialog("Phrases/LevelSeven", "Phrases/BuyLevel", "Phrases/BuyText", "Phrases/BackText", UnlockLevelSeven, null);
+    }
+
+    void UnlockLevelSeven()
+    {
+        if (PlayerPrefs.GetInt("CurrencyAmount", CurrencyControl.currencyAmount) >= 20 && PlayerPrefsBool.GetBool("Level7") == false)
+        {
+            PlayerPrefsBool.SetBool("Level7", true);
+            ls.GetComponent<LevelSelector>().UpdateLevels();
+            Userlevels.GetInstance().AddUserLevel("7");
+            PlayerPrefs.SetInt("CurrencyAmount", PlayerPrefs.GetInt("CurrencyAmount", CurrencyControl.currencyAmount) - 20);
+        }
+
+    }
+}

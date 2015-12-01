4bda28e1cf07a6aa69e1c9e980cfa430475fbbc4
Anders Wessberg <anders_wess@hotmail.com>
2015-12-01 16:10:41 +0100

    The shop updates when you buy bigger packs or many small ones or all.

--- Assets/UI/Scripts/Shop/ThreeOtherPacks.cs
+++ Assets/UI/Scripts/Shop/ThreeOtherPacks.cs
@@ -4,9 +4,9 @@ using System.Collections;
 
 public class ThreeOtherPacks : MonoBehaviour
 {
+
     public GameObject pack3, pack4, pack5;
     public Image buyButton;
-    //itemImg;
     public Sprite purchased;
     public Text costText;
     public int cost;
@@ -17,37 +17,22 @@ public class ThreeOtherPacks : MonoBehaviour
         IsBought();
     }
 
-    //void Update()
-    //{
-    //    if (PlayerPrefsBool.GetBool("Pac (3)") == true)
-    //    {
-    //        APack(pack3);
-    //    }
-    //    if (PlayerPrefsBool.GetBool("Pack (4)") == true)
-    //    {
-    //        APack(pack4);
-    //    }
-    //    if (PlayerPrefsBool.GetBool("Pack (5)") == true)
-    //    {
-    //        APack(pack5);
-    //    }
-    //}
+    void Update()
+    {
+        if (PlayerPrefsBool.GetBool("Pack (3)") == true && PlayerPrefsBool.GetBool("Pack (4)") == true && PlayerPrefsBool.GetBool("Pack (5)") == true)
+        {
+            buyButton.sprite = purchased;
+        }
+    }
 
     public void BuyThisItem()
     {
-        //if (PlayerPrefsBool.GetBool("Pack456") == true)
-        //{
-        //    APack(pack3);
-        //    APack(pack4);
-        //    APack(pack5);
-        //}
-        //else 
-        Debug.Log("FIX SÅ DEN HAKKER SINGLE PACKS AF");
         if (PlayerPrefs.GetInt("CurrencyAmount", CurrencyControl.currencyAmount) >= cost && (PlayerPrefsBool.GetBool("Pack (3)") == false || PlayerPrefsBool.GetBool("Pack (4)") == false || PlayerPrefsBool.GetBool("Pack (5)") == false))
         {
             PlayerPrefsBool.SetBool("Pack (3)", true);
             PlayerPrefsBool.SetBool("Pack (4)", true);
             PlayerPrefsBool.SetBool("Pack (5)", true);
+            PlayerPrefsBool.SetBool("Pack456", true);
             IsBought();
             PlayerPrefs.SetInt("CurrencyAmount", PlayerPrefs.GetInt("CurrencyAmount", CurrencyControl.currencyAmount) - cost);
         }

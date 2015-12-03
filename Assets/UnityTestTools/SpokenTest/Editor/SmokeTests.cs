8c9abd6ccfc8663bdf15bd1db449429b7a63e918
Server <dadiu.2015.team.4@gmail.com>
2015-12-03 16:35:26 +0100

    Unity Test Tools

---
+++ Assets/UnityTestTools/SpokenTest/Editor/SmokeTests.cs
@@ -0,0 +1,52 @@
+﻿
+using System.Collections.Generic;
+using System.Collections;
+using System.Threading;
+using NUnit.Framework;
+using UnityEngine;
+
+namespace UnityTest
+{
+	[TestFixture]
+	[Category("Smoke Tests")]
+	internal class SmokeTests
+	{
+		[Test]
+		public void RandomTest()
+		{
+			int random = Random.Range (0, 2);
+			switch (random)
+			{
+			case 0:
+				PassingTest();
+				break;
+			case 1:
+				FailingTest();
+				break;
+			default:
+				PassingTest();
+				break;
+			}
+
+			PassingTest ();
+		}
+
+
+		public void PassingTest()
+		{
+			int one = 1;
+			int two = 2;
+			int result = one + two;
+			Assert.AreEqual(3, result);
+		}
+
+	
+		public void FailingTest()
+		{
+			int one = 1;
+			int two = 2;
+			int result = one + two;
+			Assert.AreNotEqual(4, result);
+		}
+	}
+}
